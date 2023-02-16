47a1b7fa43bad6c338edca859007ecaa32db06c8
Fabiano Pires <fabiano.pires>
2023-02-16 12:20:55 -0300

    some improvement

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <v-app>
     <v-app-bar>
       <v-toolbar-title>{{ $t("app-title") }}</v-toolbar-title>
+      <template v-slot:append>
+        <v-btn to="/" icon="mdi-home"></v-btn>
+      </template>
     </v-app-bar>
     <v-main>
       <v-container>
