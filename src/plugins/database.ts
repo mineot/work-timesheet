02f1707d6ad8bd1263a1124714b58cb6ea3903bf
Fabiano Pires <fabiano.pires>
2023-02-24 14:09:20 -0300

    define localstorage like db

--- src/plugins/database.ts
+++ src/plugins/database.ts
@@ -1,13 +1,22 @@
-import "sql.js/dist/sql-wasm";
-import initSqlJs from "sql.js";
-let DB = undefined;
-
-const InitDB = async () => {
-  const SQL = await initSqlJs({
-    locateFile: () => "assets/db.sqlite"
-  });
-
-  DB = new SQL.Database();
+const separator = "->";
+const dbName = "wtsdb";
+const dbTables = ["teste"];
+const DB = {
+  teste: undefined
 };
 
-export { InitDB, DB };
+const InitDB = () => {
+  dbTables.forEach((table: string) => {
+    const data: string | null = localStorage.getItem(`${dbName}${separator}${table}`);
+    DB[table] = data ? JSON.parse(data) : [];
+  });
+};
+
+const StoreDB = () => {
+  dbTables.forEach((table: string) => {
+    const data: string = JSON.stringify(DB[table]);
+    localStorage.setItem(`${dbName}${separator}${table}`, data);
+  });
+};
+
+export { InitDB, StoreDB, DB };
